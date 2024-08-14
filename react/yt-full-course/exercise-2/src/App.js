db03226658cd67eaa4c16503a1049925d72094fa
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-14 22:32:27 +0530

    exercise -2, create separate file o keep Data

---
+++ react/yt-full-course/exercise-2/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import Main from './Components/Main';
+
+
+
+function App() {
+
+  return(
+  
+    <>
+
+      {/* <div> 
+        TDM Madushanka
+        <h1>Tharindu</h1>
+      </div>
+
+      <p>Hello</p> */}
+
+        {/* looping array by using map */}
+
+        {/* send dynimacaly array data to Main component */}
+
+      {/* {
+        myData.map(element => {
+          // return <p>{element.name}</p>
+          return <Main name={element.name} city={element.city}
+          position={element.position}/>;
+        })
+      } */}
+
+
+{/* By using Object distructuring */}
+
+  {/*
+      {
+        myData.map(({name, city,position}) => {
+          return <Main name={name} city={city}
+          position={position}/>;
+        })
+      }
+  */}
+
+  {/* to error remove in dev tools give unique key */}
+
+{
+    myData.map(({name, city,position},index) => {
+      return <Main key={index} name={name} city={city}
+      position={position}/>;
+    })
+}
+
+    </>
+
+  ); 
+}
+
+export default App;
---
+++ react/yt-full-course/exercise-2/src/Data/myData.js
@@ -0,0 +1,26 @@
+const myData = [
+    {
+      name: 'Tharindu',
+      city: 'Galle',
+      position: 'Student'
+    },
+  
+    {
+      name: 'Madushnak',
+      city: 'Matara',
+      position: 'Web Developer'
+    },
+  
+    {
+      name: 'Dilshan',
+      city: 'Colombo',
+      position: 'Full stack Developer'
+    },
+  
+    {
+      name: 'Shankar',
+      city: 'Jafna',
+      position: 'Mobile app Developer'
+    }
+  
+  ];
