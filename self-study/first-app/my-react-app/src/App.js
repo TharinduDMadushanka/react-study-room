acbf8915ae4fc0f3253ade5662a05d10a1958bd8
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-16 10:37:26 +0530

    change and test main functiond and css

--- self-study/first-app/my-react-app/src/App.js
+++ self-study/first-app/my-react-app/src/App.js
@@ -1,23 +1,27 @@
 // import logo from './logo.svg';
 import logo from './rocket.svg';
-import './App.css';
+// import './App.css';
+import './Test.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header id="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+
+          <p>Hello I'm TDM..!</p>
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
