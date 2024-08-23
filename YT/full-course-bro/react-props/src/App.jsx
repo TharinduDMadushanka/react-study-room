0d4fbbcaf6416258e27e9160d86686255dcdbf9b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-23 15:46:20 +0530

    react props for pasing values

---
+++ YT/full-course-bro/react-props/src/App.jsx
@@ -0,0 +1,22 @@
+import Student from "./Student"
+
+function App() {
+
+  return (
+    <>
+      <Student name="TDM" age={20} isStudent={true}/>
+      <Student name="Shankar" age={42} isStudent={false}/>
+      <Student name="Perera" age={50} isStudent={false}/>
+      <Student name="Madushanka" age={18} isStudent={true}/>
+    </>
+  )
+}
+
+export default App
+
+/**
+ * Props
+  * read-only properties that are sgared between cmponents
+  * A parent component can send data to a chld component
+  * <Component key=value />
+ */
