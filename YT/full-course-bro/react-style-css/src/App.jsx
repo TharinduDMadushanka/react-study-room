ca97eb9f12acdf2e83fcc12be01e2a13f02dd2d9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-23 14:55:12 +0530

    study how apply style component with css & do external css

---
+++ YT/full-course-bro/react-style-css/src/App.jsx
@@ -0,0 +1,20 @@
+import Button from "./Button"
+
+function App() {
+
+
+  return (
+    <>
+      <Button/>
+    </>
+  )
+}
+
+export default App
+
+/**
+ * We can style react component with CSS
+  * External
+  * Modules
+  * Inline
+ */
