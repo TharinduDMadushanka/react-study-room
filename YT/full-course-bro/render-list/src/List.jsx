bd5255ed619997b6784c1c041ea95f8d81ec9e54
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-23 18:57:09 +0530

    render lists with sort method

---
+++ YT/full-course-bro/render-list/src/List.jsx
@@ -0,0 +1,34 @@
+
+function List(){
+
+    const fruits = [{id: 1, name :"apple", calories: 95},
+                    {id: 2, name: "banana", calories: 45},
+                    {id: 3, name :"orange", calories: 105},
+                    {id: 4, name :"coconut", calories:115 },
+                    {id: 5, name :"mango", calories: 45}];
+
+    // const listItems = fruits.map(fruit => <li>{fruit}</li>);
+
+    // Alphabetical sort
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)); 
+
+    // reverse Alphabetical
+    // fruits.sort((a,b) => b.name.localeCompare(a.name)); 
+
+    //Numerica order
+    // fruits.sort((a,b) => a.calories - b.calories);
+
+    //reverse Numerica order
+    fruits.sort((a,b) => b.calories - a.calories);
+
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                            {fruit.name}: &nbsp;
+                                            <b>{fruit.calories}</b></li>);
+
+    // return(fruits);
+    // return(<ul>{listItems}</ul>);
+    return(<ol>{listItems}</ol>);
+
+}
+
+export default List
