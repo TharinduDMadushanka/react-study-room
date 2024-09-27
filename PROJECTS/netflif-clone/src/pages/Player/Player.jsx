a243c6bf5634fd2046a19568f6b5a6bd5dd7afed
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-27 10:34:52 +0530

    create Player component for display the trailer by fetching data from TMDB API

--- PROJECTS/netflif-clone/src/pages/Player/Player.jsx
+++ PROJECTS/netflif-clone/src/pages/Player/Player.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_aarow from '../../assets/back_arrow_icon.png'
 
+// get youtube trailer video and its details by video id using tmdb api [get movie id & movies -> video and paste that id and you will get the details for respective video]
 const Player = () => {
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  });
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjZlNDJjY2FmMjY3MDRkMTRmNGIxNzRlYzczYzZjYyIsIm5iZiI6MTcyNzQxMDI2Ny4xNTI2OTIsInN1YiI6IjY2ZjYyZjM5NmNlZGJlZDAxNTcwYTNjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KfgvWUhqRzgWi-Ac_SiRhf1sK8pwv9ktoZjCg2W5COM'
+    }
+  };
+
+  useEffect(()=>{
+    fetch('https://api.themoviedb.org/3/movie/957452/videos?language=en-US', options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0])) // store the details of fetched data results first object
+    .catch(err => console.error(err));
+
+  },[])
+  
+  
   return (
-    <div>
-      
+
+    <div className='player'>
+      <img src={back_aarow} alt="" />
+      <iframe width='90%' height='90%' 
+        src= {`https://www.youtube.com/embed/${apiData.key}`}  // set respective video traile by passing fetch datas key 
+        title='trailer' frameBorder='0' allowFullScreen> {/* url fr movie trailer(yt pattern) */}
+      </iframe>
+
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.typeof}</p>
+      </div>
     </div>
   )
 }
