79503c9b1667a19dbb7fa772ff393404ac802413
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-27 11:25:36 +0530

    install firebase pkg and creates firebase.js file

---
+++ PROJECTS/netflif-clone/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Qs-Ou729xqQPRYdgsNDJGfGIUhMzHjI",
+  authDomain: "netflix-clone-1bb90.firebaseapp.com",
+  projectId: "netflix-clone-1bb90",
+  storageBucket: "netflix-clone-1bb90.appspot.com",
+  messagingSenderId: "363028733993",
+  appId: "1:363028733993:web:62a7f14df6590240f7bc38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
