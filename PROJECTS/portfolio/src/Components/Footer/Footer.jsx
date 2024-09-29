8e4e3ebb6c04ab29e4b8e59ecd1434581ead0267
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-29 10:32:38 +0530

    completed Footer section

---
+++ PROJECTS/portfolio/src/Components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon.svg'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        
+        <div className="footer-top">
+            <div className="footer-top-left">
+                <img src={footer_logo} alt="" />
+                <p>
+                  I’m excited to keep growing and learning in this ever-evolving field of technology. 
+                  Stay connected for future updates and projects.
+                </p>
+            </div>
+
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <img src={user_icon} alt="" />
+                    <input type="email" placeholder='Enter your email'/>
+                </div>
+                <div className="footer-subscribe">Subscribe</div>
+            </div>
+        </div>
+        <hr />
+
+        <div className="footer-bootom">
+            <p className="footer-bottom-left">@ 2023 Alex Bennet.All rights reserved.</p>
+            <div className="footer-bottom-right">
+                <p>Term of Services</p>
+                <p>Privacy Policy</p>
+                <p>Connect with me</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
