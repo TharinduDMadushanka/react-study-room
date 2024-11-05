7567899afdacf5ec4144858c4821df36050c0738
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-11-05 12:07:40 +0530

    created form for adding new blogs

--- full-stack/next-blog-app/app/admin/addProduct/page.jsx
+++ full-stack/next-blog-app/app/admin/addProduct/page.jsx
@@ -10,11 +10,29 @@ const page = () => {
   return (
     <>
       <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+
           <p className='text-xl'>Upload thumbnail</p>
           <label htmlFor="image">
             <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} />
           </label>
           <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required />
+
+          <p className='text-xl mt-4'>Blg title</p>
+          <input className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required/>
+
+          <p className='text-xl mt-4'>Blg description</p>
+          <textarea className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='write content here' rows={6} required/>
+
+          <p className='text-xl mt-4'>Blog category</p>
+          <select name="category" className='w-40 mt-4 px-4 py-3 border text-gray-500'>
+            <option value="Startup">Startup</option>
+            <option value="Technology">Technology</option>
+            <option value="Lifestyle">Startup</option>
+          </select>
+          <br />
+
+          <button className='mt-8 w-40 h-12 bg-black text-white' type="submit">Add</button>
+
       </form>
     </>
   )
