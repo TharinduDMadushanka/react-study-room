16f6a8324055f23b753998e7eefd87df02162629
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-11-05 11:59:19 +0530

    comlete image upload logic

--- full-stack/next-blog-app/app/admin/addProduct/page.jsx
+++ full-stack/next-blog-app/app/admin/addProduct/page.jsx
@@ -1,11 +1,22 @@
 'use client'
-import React from 'react'
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 const page = () => {
+
+  const [image, setImage] = useState(false);
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+          <p className='text-xl'>Upload thumbnail</p>
+          <label htmlFor="image">
+            <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} />
+          </label>
+          <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required />
+      </form>
+    </>
   )
   
 }
